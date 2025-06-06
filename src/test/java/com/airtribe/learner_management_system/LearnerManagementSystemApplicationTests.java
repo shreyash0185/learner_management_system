00527e714cd48bd3678616cda33dd37f4942d45e
shreyash0185 <shreyashmuktalkar98@gmail.com>
2025-06-06 12:03:07 +0530

    Initial commitlearner_management_system

---
+++ src/test/java/com/airtribe/learner_management_system/LearnerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtribe.learner_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
